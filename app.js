13c3c2defc5b2cd32b56761b2b3fd50cbc66569b
TAPAN-Agrawal <taapanagrawal2002@gmail.com>
2023-06-16 14:09:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require("http")
+const express=require("express")
+const app=express()
+const bodyParser=require("body-parser")
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>');
+});
+const server = http.createServer(app);
+
+server.listen(3000)
